247857ee323766d6fd92c452056b85c053fca615
Zingerur <poralo1995@gmail.com>
2025-12-05 18:54:26 +0200

    Add automatic migrate and superuser

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
+release: python manage.py migrate
+web: gunicorn locallibrary.wsgi
+
+release: python manage.py migrate && python manage.py shell < locallibrary/create_superuser.py
 web: gunicorn locallibrary.wsgi
---
+++ locallibrary/create_superuser.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        password="fadar321",
+        email="poralo1995@gmail.com"
+    )
